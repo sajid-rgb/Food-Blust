2618bf502d9a42f256d05a6bd7453a06dc97a34a
Sajid Chowdhury <msrchk@gmail.com>
2021-06-04 00:35:15 +0600

    navbar updated

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
+import { Home, OrderDelivery, Restaurent } from './components/screens';
+import { Tabs } from './navigation/tab';
 
+const Stack =createStackNavigator()
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+     <NavigationContainer>
+       <Stack.Navigator  screenOptions={{
+           headersShown:false
+         }} initialValue={'Home'}
+         >
+         <Stack.Screen name="Home" component={Tabs}/>
+         <Stack.Screen name="Restaurent" component={Restaurent}/>
+         <Stack.Screen name="Order Delivery" component={OrderDelivery}/>
+         
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+         
+       </Stack.Navigator>
+     </NavigationContainer>
+  )
+
+}
---
+++ components/screens/Restaurent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+export const Restaurent =()=>{
+
+  return (
+    <View>
+    <Text>
+    Restaurent
+
+    </Text>
+
+    </View>
+  )
+}
