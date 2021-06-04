a6af97061f414e80157491a851f7dd86ee6fc871
Sajid Chowdhury <msrchk@gmail.com>
2021-06-04 21:59:32 +0600

    last push

--- navigation/tab.js
+++ navigation/tab.js
@@ -14,7 +14,7 @@ import { Home } from '../components/screens/Home';
 const Tab = createBottomTabNavigator()
 
 const TabBarCustomButton =({accessibilityState,children,onPress})=>{
-  var isSelected = accessibilityState.selected
+  let isSelected = accessibilityState.selected
   if (isSelected){
     return(
                   <View style={{ flex: 1, alignItems: "center" }}>
